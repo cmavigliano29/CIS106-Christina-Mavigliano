61f43ab92ce2590403685e899fced25f232613a1
cmavigliano29 <mc87923@mail.harpercollege.edu>
2024-09-15 22:54:39 -0500

    Add files via upload

---
+++ Week 3/ps2p5.py
@@ -0,0 +1,10 @@
+#input phase
+radius = float(input("Enter radius of circle"))
+
+#process phase
+area = 3.174 * radius * radius
+perimeter = 2 * 3.174 * radius
+
+#output phase
+print("Area of circle: ", area)
+print("Perimeter of circle: ", perimeter)
