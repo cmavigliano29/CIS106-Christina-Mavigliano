14119fa8aa881e46f2580a4eaf3f57e1108068d0
cmavigliano29 <mc87923@mail.harpercollege.edu>
2024-09-23 12:29:27 -0500

    Add files via upload

---
+++ Week 4/ps3p2.py
@@ -0,0 +1,16 @@
+#input phase
+item = input("Enter item name ")
+qty = float(input("Enter quantity of item "))
+
+#process phase
+if qty == "A":
+    unitprice = 10.00
+else:
+    unitprice = 20.00
+
+extendedprice = qty * unitprice
+
+#output phase
+print("Item:" , item )
+print("Unit price:" , unitprice)
+print("Extended price:" , extendedprice)
