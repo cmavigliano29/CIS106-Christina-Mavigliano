ed097d23520c2861af4f2b93871a976af7570326
cmavigliano29 <mc87923@mail.harpercollege.edu>
2024-12-08 05:32:51 -0600

    Add files via upload

---
+++ Week 6/ps5p2.py
@@ -0,0 +1,9 @@
+#input phase
+start = float(input("Enter start value "))
+stop = float(input("Enter stop value "))
+increment = float(input("Enter increment value "))
+
+#process phase
+while(start<=stop):
+  print(start)
+  start = start + increment
